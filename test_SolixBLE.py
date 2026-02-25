a8be7232bba4966ae5b50ecf8121ff37e983375b
Harvey Lelliott <42912136+flip-dots@users.noreply.github.com>
2026-02-25 18:38:09 +0000

    Add tests for C1000 parsing

---
+++ test_SolixBLE.py
@@ -0,0 +1,94 @@
+"""
+Tests for SolixBLE module.
+"""
+
+from typing import Any
+
+import pytest
+from bleak import BLEDevice
+
+from SolixBLE import C1000, PortStatus, SolixBLEDevice
+
+MOCK_DEVICE_NAME = "Mock Device"
+MOCK_DEVICE_ADDRESS = "AA:BB:CC:DD:EE:FF"
+MOCK_BLE_DEVICE = BLEDevice(MOCK_DEVICE_ADDRESS, MOCK_DEVICE_NAME, {})
+
+
+@pytest.mark.parametrize(
+    "device_class,data,mapping",
+    [
+        pytest.param(
+            C1000,
+            "a10131a2050300000000a3050300000000a403026b06a503020000a603020000a703020000a803020000a903020000aa03020000ab03020000ac03020000ad03020000ae03020000af03020000b003020100b103020000b203020000b30302a600b403020000b503020000b60302ff01b703020000b803029a00b903020000ba0302a600bb03020000bc020100bd020117be020100bf020101c0020100c1020157c2020100c3020164c4020100c5020100c6020100c7020100c8020100c9020100ca020100cb020100cc020100cd020100ce020100cf020100d0110041504339464530453237333030323735e5020100",
+            {
+                "ac_timer_remaining": 0,
+                "ac_timer": None,
+                "hours_remaining": 10.7,
+                "days_remaining": 6,
+                "ac_power_in": 0,
+                "ac_power_out": 0,
+                "usb_c1_power": 0,
+                "usb_c2_power": 0,
+                "usb_a1_power": 0,
+                "usb_a2_power": 0,
+                "solar_power_in": 0,
+                "power_in": 0,
+                "power_out": 1,
+                "software_version": "1.6.6",
+                "software_version_expansion": "0",
+                "software_version_controller": "1.6.6",
+                "ac_on": False,
+                "solar_port": PortStatus.NOT_CONNECTED,
+                "temperature": 23,
+                "battery_percentage": 87,
+                "serial_number": "APC9FE0E27300275",
+            },
+            id="c1000_idle",
+        ),
+        pytest.param(
+            C1000,
+            "a10131a2050300000000a3050300000000a403020800a503020000a60302d203a703020000a803020000a903020000aa03020000ab03020000ac03020000ad03020000ae03020000af03020000b00302d303b103020000b203020000b30302a600b403020000b50302ff01b60302ff01b703020000b803029a00b903020000ba0302a600bb03020100bc020100bd02011abe020100bf020101c0020100c102014fc2020100c3020164c4020100c5020100c6020100c7020100c8020100c9020100ca020100cb020100cc020100cd020100ce020100cf020100d0110041504339464530453237333030323735e5020100",
+            {
+                "ac_timer_remaining": 0,
+                "ac_timer": None,
+                "hours_remaining": 0.8,
+                "days_remaining": 0,
+                "ac_power_in": 0,
+                "ac_power_out": 978,
+                "usb_c1_power": 0,
+                "usb_c2_power": 0,
+                "usb_a1_power": 0,
+                "usb_a2_power": 0,
+                "solar_power_in": 0,
+                "power_in": 0,
+                "power_out": 979,
+                "software_version": "1.6.6",
+                "software_version_expansion": "0",
+                "software_version_controller": "1.6.6",
+                "ac_on": True,
+                "solar_port": PortStatus.NOT_CONNECTED,
+                "temperature": 26,
+                "battery_percentage": 79,
+                "serial_number": "APC9FE0E27300275",
+            },
+            id="c1000_ac_load",
+        ),
+    ],
+)
+def test_values(
+    device_class: SolixBLEDevice, data: str, mapping: dict[str, Any]
+) -> None:
+    """
+    Test that a decrypted packet is parsed into the correct values.
+
+    :param device_class: Class of device under test.
+    :param data: The raw decrypted telemetry bytes.
+    :param mapping: Mapping of class properties to their expected value.
+    """
+    device = device_class(MOCK_BLE_DEVICE)
+    device._data = bytes.fromhex(data)
+
+    for class_property, expected_value in mapping.items():
+        assert (
+            getattr(device, class_property) == expected_value
+        ), f"Mismatch for property '{class_property}'!"
